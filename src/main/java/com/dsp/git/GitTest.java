8ceb425316acb8f8f82d5f0915c8892f951c945b
richard <richard@163.com>
2021-10-20 19:53:51 +0800

    second commit

--- src/main/java/com/dsp/git/GitTest.java
+++ src/main/java/com/dsp/git/GitTest.java
@@ -3,5 +3,6 @@ package com.dsp.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!!!");
+        System.out.println("hello git2!!!");
     }
 }
